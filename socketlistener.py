5c0e94a4d2589b2df6ebd94e570d2984a98fd770
Sanjeev Sahoo <sks90@sfu.ca>
2022-04-11 00:56:03 -0700

    listeners

--- socketlistener.py
+++ socketlistener.py
@@ -5,22 +5,10 @@ from snortunsock import snort_listener
 
 
 def mac_addr(address):
-    """Convert a MAC address to a readable/printable string
-       Args:
-           address (str): a MAC address in hex form (e.g. '\x01\x02\x03\x04\x05\x06')
-       Returns:
-           str: Printable/readable MAC address
-    """
     return ':'.join('%02x' % ord(b) for b in address)
 
 
 def ip_to_str(address):
-    """Print out an IP address given a string
-    Args:
-        address (inet struct): inet network address
-    Returns:
-        str: Printable/readable IP address
-    """
     return socket.inet_ntop(socket.AF_INET, address)
 
 
@@ -28,19 +16,12 @@ def main():
     for msg in snort_listener.start_recv("/tmp/snort_alert"):
         print('alertmsg: %s' % ''.join(msg.alertmsg))
         buf = msg.pkt
-
-        # Unpack the Ethernet frame (mac src/dst, ethertype)
         eth = dpkt.ethernet.Ethernet(buf)
         print('Ethernet Frame: ', mac_addr(eth.src), mac_addr(eth.dst), eth.type)
 
         if eth.type != dpkt.ethernet.ETH_TYPE_IP:
             print('Non IP Packet type not supported %s\n' % eth.data.__class__.__name__)
-    
-        # Now unpack the data within the Ethernet frame (the IP packet)
-        # Pulling out src, dst, length, fragment info, TTL, and Protocol
         ip = eth.data
-
-        # Pull out fragment information (flags and offset all packed into off field, so use bitmasks)
         do_not_fragment = bool(ip.off & dpkt.ip.IP_DF)
         more_fragments = bool(ip.off & dpkt.ip.IP_MF)
         fragment_offset = ip.off & dpkt.ip.IP_OFFMASK
